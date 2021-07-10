94b38ab3fe0f2cd2af85826465a0eb384ecd5fc7
yashlalpotu11 <yashrlalpotu30@gmail.com>
2021-07-11 00:01:20 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+// import Router from './routes/Router'
+
+const App = () => {
+  return (
+    <div>
+      {/* <Router /> */}
+      <h1>Yash</h1>
+    </div>
+  )
+}
+
+export default App;
